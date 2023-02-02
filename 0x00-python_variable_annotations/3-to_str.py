014855cb0dae44a6663525d57cbdedaf2666dec7
kezzyj <karachidavid31@gmail.com>
2023-02-02 17:46:06 +0000

    take float return str

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""Contains an annotated func that changes a float to a string"""
+
+
+def to_str(n: float) -> str:
+
+    """Func returning str of a float"""
+
+    return str(n)
