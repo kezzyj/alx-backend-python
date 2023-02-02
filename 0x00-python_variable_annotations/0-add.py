3b5310e8e3aa5efc124e556c731241806a5ad937
kezzyj <karachidavid31@gmail.com>
2023-02-02 17:34:38 +0000

    addint numbers with annotations

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""Adds two floats together.on Ubuntu 18.04 LTS using python3 (version 3.7)"""
+
+
+
+
+
+def add(a: float, b: float) -> float:
+
+    """Adds two floats together"""
+
+    return a + b
