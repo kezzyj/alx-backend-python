32a828bbc29306b44fb3b273df55952058537a82
kezzyj <karachidavid31@gmail.com>
2023-02-02 17:56:54 +0000

    annotation of list of floats

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""Contains a function that return a float sum of the numbers found in the
+arg - input_list"""
+
+
+from typing import List
+
+input_list = List[float]
+
+
+def sum_list(input_list: input_list) -> float:
+
+    """Returns sum of the floats found in the input_List arg"""
+
+    return sum(input_list)
