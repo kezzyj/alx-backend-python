cb92a32d9b9bafa436ac34ac7e3f8b7467db05ec
kezzyj <karachidavid31@gmail.com>
2023-02-02 17:41:30 +0000

    accept float return floor (int)

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+""" Basic annotations - floor """
+
+
+def floor(n: float) -> int:
+
+    """
+
+    Parameters
+
+    ----------
+
+    n: float
+
+    Return: the floor of the float
+
+    """
+
+    return int(n)
