6a34797fae99020572b96621a4d112f7e7a66bd6
kezzyj <karachidavid31@gmail.com>
2023-02-02 18:52:27 +0000

    iterable sequence

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+""" Let's duck type an iterable object """
+
+from typing import List, Iterable, Sequence, Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+
+    """
+
+    Parameters
+
+    ----------
+
+    lst: Iterable
+
+    Return: list
+
+    """
+
+    return [(i, len(i)) for i in lst]
