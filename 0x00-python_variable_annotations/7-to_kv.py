12927d7fcc5f8dcf83d1d169227251e960922e4d
kezzyj <karachidavid31@gmail.com>
2023-02-02 18:48:57 +0000

    fixing pycodestyle

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -22,4 +22,3 @@ def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
 
     return (k, v ** 2)
-
---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+""" Complex types - functions """
+
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+
+    """
+
+    Parameters
+
+    ----------
+
+    multiplier: float
+
+    Return: a function that multiplies a float by multiplier
+
+    """
+
+    return lambda n: n * multiplier
